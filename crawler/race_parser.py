d7ae008b8913cd14200019a709c14b5ddae31b18
otomarukanta <kanta208@gmail.com>
2016-02-28 21:21:21 +0900

    Change variables into dictionary for regex

--- crawler/race_parser.py
+++ crawler/race_parser.py
@@ -2,15 +2,16 @@ import re
 from datetime import datetime as dt
 from datetime import date
 
-regex_race_id = re.compile("race\/result\/(\d+)")
-regex_path = re.compile("/\w+/\w+/(\d+)/")
-regex_sex_age = re.compile("(\w+?)(\d+)")
-regex_jockey_weight = re.compile("\w*?(\d+\.\d+)")
-regex_weight = re.compile("(\d*)\((.*?)\)")
-regex_day = re.compile(r"(\d*)年(\d*)月(\d*)日.*?(\d*)回"
+regex = dict()
+regex['race_id'] = re.compile("race\/result\/(\d+)")
+regex['path'] = re.compile("/\w+/\w+/(\d+)/")
+regex['sex_age'] = re.compile("(\w+?)(\d+)")
+regex['jockey_weight'] = re.compile("\w*?(\d+\.\d+)")
+regex['weight'] = re.compile("(\d*)\((.*?)\)")
+regex['day'] = re.compile(r"(\d*)年(\d*)月(\d*)日.*?(\d*)回"
                        "(.*?)(\d*)日.*?(\d*:\d*)発走")
-regex_title = re.compile(r"競馬 - (.+?) 結果 - スポーツナビ")
-regex_meta = re.compile(r"(.+?)・(.+?) (\d+?)m \[コースガイド\]"
+regex['title'] = re.compile(r"競馬 - (.+?) 結果 - スポーツナビ")
+regex['meta'] = re.compile(r"(.+?)・(.+?) (\d+?)m \[コースガイド\]"
                         " \| 天気： \| 馬場： \| (.+?) \| (.+?)"
                         " \| (.+?) \|")
 grades = ['新馬', '未勝利', '500万下', '900万下',
@@ -30,21 +31,21 @@ def parse_race_result(soup):
             res['final_position'] = record[0].text
             res['frame_number'] = record[1].text
             res['horse_number'] = record[2].text
-            res['horse_id'] = regex_path.findall(record[3].a['href'])[0]
-            res['sex'], res['age'] = regex_sex_age.findall(record[4].text)[0]
-            res['jockey_id'] = regex_path.findall(record[5].a['href'])[0]
-            res['jockey_weight'] = regex_jockey_weight.findall(
+            res['horse_id'] = regex['path'].findall(record[3].a['href'])[0]
+            res['sex'], res['age'] = regex['sex_age'].findall(record[4].text)[0]
+            res['jockey_id'] = regex['path'].findall(record[5].a['href'])[0]
+            res['jockey_weight'] = regex['jockey_weight'].findall(
                 record[10].text)[0]
             if res['final_position'].strip() == '取消':
                 return {k: v.strip() for k, v in res.items()}
-            weight = regex_weight.findall(record[11].text)[0][0]
+            weight = regex['weight'].findall(record[11].text)[0][0]
             if weight:
                 res['horse_weight'] = weight
             else:
                 res['horse_weight'] = '-1'
             if res['final_position'].strip() == '除外':
                 return {k: v.strip() for k, v in res.items()}
-            weight = regex_weight.findall(record[11].text)[0][0]
+            weight = regex['weight'].findall(record[11].text)[0][0]
             res['odds'] = record[13].text
             res['blinker'] = record[14].text
             if res['final_position'].strip() == '中止':
@@ -72,7 +73,7 @@ def parse_race_result(soup):
     def parse_info():
         # race_info
         res = dict()
-        schedule = regex_day.findall(
+        schedule = regex['day'].findall(
             soup.find(id='raceTitDay').text)[0]
         res['race_date'] = date(int(schedule[0]), int(schedule[1]),
                                 int(schedule[2])).isoformat()
@@ -80,14 +81,14 @@ def parse_race_result(soup):
         res['place'] = schedule[4]
         res['days'] = schedule[5]
         res['start_time'] = schedule[6]
-        res['race_name'] = regex_title.findall(soup.title.text)[0]
+        res['race_name'] = regex['title'].findall(soup.title.text)[0]
 
         weather = soup.find_all(class_='spBg')
         res['weather'] = weather[0]['alt']
         res['track_condition'] = weather[1]['alt']
 
         # meta
-        meta = regex_meta.findall(
+        meta = regex['meta'].findall(
             soup.find(id='raceTitMeta').text)[0]
         res['track_type'] = meta[0]
         res['rotation'] = meta[1]
@@ -128,7 +129,7 @@ def parse_race_result(soup):
         'meta', attrs={'property': 'og:url'})[0]['content']
     if any(x in url for x in ['denma', 'regist']):
         return
-    race_id = regex_race_id.findall(url)[0]
+    race_id = regex['race_id'].findall(url)[0]
     res['result'] = parse_result()
     for i in range(len(res['result'])):
         res['result'][i]['race_id'] = race_id
