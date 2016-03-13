4240243c296aa7a1d04d585e79fbfde764f25df0
otomarukanta <kanta208@gmail.com>
2016-03-13 23:02:41 +0900

    Modify cacheDB to use bz2 compression

--- crawler/cachedb.py
+++ crawler/cachedb.py
@@ -1,11 +1,13 @@
 import os
 from datetime import datetime
 import dbm
+import codecs
 
 
 class CacheDB():
 
     _db_filename = './cache.dbm'
+    _comp_format = 'bz2'
 
     def __init__(self):
         pass
@@ -17,8 +19,10 @@ class CacheDB():
         print('Get page from cache in {} at {}'.format(path, datetime.now()))
         with dbm.open(self._db_filename, 'c') as db:
             page = db.get(path)
-        return page
+        if page is None:
+            return None
+        return codecs.decode(page, self._comp_format).decode()
 
     def put(self, path, page):
         with dbm.open(self._db_filename, 'c') as db:
-            db[path] = page
+            db[path] = codecs.encode(page.encode(), self._comp_format)
